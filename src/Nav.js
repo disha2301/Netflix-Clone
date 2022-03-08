e49a923e88514aba2d356926fe79c5a93ba89f0d
disha2301 <91051022+disha2301@users.noreply.github.com>
2022-03-08 21:42:39 +0530

    first commit

---
+++ src/Nav.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://1000logos.net/wp-content/uploads/2017/05/Netflix-Logo.png"
+        alt="Netflix Logo"
+      />
+      <img
+        className="nav__avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="Netflix user logo"
+      />
+    </div>
+  );
+}
+
+export default Nav;
